dee4e083da15e112c3bcf4eac939cfc556cd95f7
msdr9 <msdr9@yahoo.com>
2024-07-13 23:03:54 +0530

    updaed backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "test-resources"
+    resource_group_name  = "sdrrg"
     storage_account_name = "techtutorialwthmsdr"
     container_name       = "prod-tfstate"
     key                  = "prod.terraform.tfstate"
